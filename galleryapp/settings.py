90d568c26b7de78d007481389459ebfc47796334
roxaline <mariadelarosita078@gmail.com>
2019-03-18 17:35:30 +0200

    add media link

--- galleryapp/settings.py
+++ galleryapp/settings.py
@@ -118,6 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
@@ -125,3 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
